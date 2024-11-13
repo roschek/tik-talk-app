d9c144d3a79b5ef6e6341b3d41eac0be94384868
alexk <alexk@gmail.com>
2024-11-13 05:31:06 +0200

    add auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import {LoginPageComponent} from './pages/login-page/login-page.component';
 import {SearchPageComponent} from './pages/search-page/search-page.component';
+import {ProfilePageComponent} from './pages/profile-page/profile-page.component';
+import {LayoutComponent} from './common-ui/layout/layout.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
-  {path: '', component: SearchPageComponent},
+  {path: '', component: LayoutComponent, children: [
+      {path: '', component: SearchPageComponent},
+      {path: 'profile', component: ProfilePageComponent}
+    ]},
 ];
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-
+  authService = inject(AuthService);
+  form = new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+  })
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+    //@ts-ignore
+    this.authService.login(this.form.value).subscribe((response) => {
+  });
+  }
 }
